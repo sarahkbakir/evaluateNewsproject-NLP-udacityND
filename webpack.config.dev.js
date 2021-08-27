b2b3f29eb87a4957443dfbe015b32185b1e97e81
Sarah Bakir <sarahbakir.k@gmail.com>
2021-08-27 20:12:17 +0200

    webpack config - build-dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
@@ -31,15 +28,6 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
         }),
         new WorkboxPlugin.GenerateSW()
     ]
